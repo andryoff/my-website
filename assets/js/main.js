0532b22306d6e01154883881fa75ce195e7560f3
Andry Taranto <andrytaranto@gmail.com>
2026-04-15 14:13:18 +0200

    ultime modifiche grafiche

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,40 @@
     });
   });
 
+/**
+   * Nav click and scroll to section with active state
+   */
+const navLinks = document.querySelectorAll(".navmenu a");
+
+// CLICK 
+navLinks.forEach(link => {
+  link.addEventListener("click", function () {
+    navLinks.forEach(l => l.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+
+// SCROLL 
+window.addEventListener("scroll", () => {
+  let sections = document.querySelectorAll("section");
+  let current = "";
+
+  sections.forEach(section => {
+    let sectionTop = section.offsetTop - 120;
+    if (window.scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
+
+
   /**
    * Preloader
    */
